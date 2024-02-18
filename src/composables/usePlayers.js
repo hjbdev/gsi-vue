fd647443e9a10402e79735d2b29d513bdefb7c7a
Harry Bayliss <30434619+hjbdev@users.noreply.github.com>
2024-02-18 23:40:16 +0000

    prevented new computed var being created every time usePlayers is called with a param

--- src/composables/usePlayers.js
+++ src/composables/usePlayers.js
@@ -5,14 +5,25 @@ const allplayers = computed(() => {
     return useGSI().value?.allplayers ?? null;
 });
 
+const cts = computed(() => {
+    return Object.values(allplayers.value ?? []).filter((player) => player.team === 'CT') ?? null;
+});
+
+const ts = computed(() => {
+    return Object.values(allplayers.value ?? []).filter((player) => player.team === 'T') ?? null;
+});
+
+
+
 /**
  * @returns {import("vue").ComputedRef<import("./useGSI.js").PlayerList | null>} All players in the game.
  */
 export default function usePlayers(team = null) {
-    if (team) {
-        return computed(() => {
-            return Object.values(allplayers.value ?? []).filter((player) => player.team === team) ?? null;
-        });
+    if (team === 'CT') {
+        return cts;
+    } else if (team === 'T') {
+        return ts;
     }
+    
     return allplayers;
 }
