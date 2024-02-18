95b579d7a5ca027a17214b61a8395d1fe7dcd70c
Harry Bayliss <30434619+hjbdev@users.noreply.github.com>
2024-02-18 20:14:34 +0000

    fix: silly silly v2

--- src/composables/usePlayer.js
+++ src/composables/usePlayer.js
@@ -2,10 +2,10 @@ import { computed } from "vue";
 import useGSI from "./useGSI";
 
 /**
- * @returns {import("vue").ComputedRef<import("./useGSI.js").Player>} The current player.
+ * @returns {import("vue").ComputedRef<import("./useGSI.js").Player | null>} The current player.
  */
 export default function usePlayer() {
     return computed(() => {
-        return useGSI().value?.player;
+        return computed(() => useGSI().value?.player ?? null);
     });
 }
